2244b31a66917afee73ab74ecb857c939ac5c081
fredtruix <fredricktrust@gmail.com>
2023-04-25 08:35:06 +0100

    add: all models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,62 @@
+from email.policy import default
 from django.db import models
+from users.models import User
+
 
 # Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+
+class Blog(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    images = models.ImageField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+    views = models.ManyToManyField(User, related_name="view", blank=True)
+    likes = models.ManyToManyField(User,related_name="like", blank=True )
+
+
+    def __str__(self):
+        return f"{self.user} just post {self.title}"
+
+
+
+
+
+class Group(models.Model):
+    host =  models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name="participant", blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
+
+
+    def __str__(self):
+        return self.name
+
+
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    # class Meta:
+    #     ordering = ['-updated', '-created']
+
+    def __str__(self):
+        return self.body[0:50] or "Message"
